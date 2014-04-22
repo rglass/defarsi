f5551812be9436155fc98cd800c6b335f81bbfc9
Roman Glass <n1sht4@gmail.com>
2014-04-22 19:14:52 +0300

    added letters

--- src/farsi_trans/core.clj
+++ src/farsi_trans/core.clj
@@ -1,16 +1,46 @@
 (ns farsi-trans.core)
 
-(defn transcribe-letter [l r_word]
-  (if (not (nil? l))
-    (str (cond
-          (= l \u0627) \a
-          (= l \u0628) \b
-          (= l \u0635) \s
-          (= l \u062F) \d
-          (= l \u06CC) \y)
-         (if (not (nil? r_word))
-           (transcribe-letter (first r_word) (rest r_word))))))
+(defn decimal-to-hex [x]
+  (-> x
+      Integer/parseInt
+      (Integer/toString 16)
+      .toUpperCase))
 
-(defn transcribe [f_word]
-  (transcribe-letter (first f_word) (rest f_word)))
+(def conversion-table
+  { \u0627 \a
+    \u0628 \b
+    \u067E \p
+    \u062A \t
+    \u0630 \z
+    \u0631 \r
+    \u0632 \z
+    \u062C \c
+    \u062D \h
+    \u067D \s
+    \u0648 \u
+    \u0633 \s
+    \u0634 \ş
+    \u0635 \s
+    \u0643 \k
+    \u0646 \n
+    \u0647 \h
+    \u0637 \t
+    \u0639 \h
+    \u063A \ğ
+    \u062F \d
+    \u062E \h
+    \u0686 \ç
+    \u0698 \j
+    \u06AF \g
+    \u06CC \y
+    \u064A \y})
+
+(defn transcribe-letter [l r-word]
+  (if (not (nil? l))
+    (str (get conversion-table l)
+         (if (not (nil? r-word))
+           (transcribe-letter (first r-word) (rest r-word))))))
+
+(defn transcribe [f-word]
+  (transcribe-letter (first f-word) (rest f-word)))
 
