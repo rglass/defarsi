1f298685003c976d91627ffdcd64856b40d48f9b
Roman Glass <n1sht4@gmail.com>
2014-04-23 13:17:50 +0300

    cljs

--- project.clj
+++ project.clj
@@ -1,7 +1,14 @@
 (defproject farsi-trans "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main farsi-trans.core)
+  :url "https://github.com/rglass/defarsi"
+  :license {:name "GPLv3"
+            :url "http://www.gnu.org/licenses/"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1934"]
+                 [domina "1.0.2"]]
+  :source-paths ["src/clj"]
+  :plugins [[lein-cljsbuild "0.3.4"]]
+  :cljsbuild {:builds[{:source-paths ["src/cljs"],
+                       :compiler {:output-to "resources/public/defarsi.js"
+                                  :optimizations :whitespace}}]}
+)
